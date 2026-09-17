58f06c585f4e395a74ba3374eb6d0eb04239de2e
redisnotbluedev <red@redisnot.blue>
2026-09-18 08:44:06 +0930

    scrap it

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const body = document.body;
-let lastScroll = window.scrollY;
 
 window.addEventListener("scroll", () => {
-	const current = window.scrollY;
-	if (current > lastScroll) {
+	if (body.scrollTop > 0) {
 		body.classList.toggle("down", true);
-	}
-	if (current < lastScroll) {
+	} else {
 		body.classList.toggle("down", false);
 	}
-
-	lastScroll = window.scrollY;
 });
