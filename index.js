df17b5f43fc5bb785de618ca8b09e9cf221b19ee
redisnotbluedev <red@redisnot.blue>
2026-09-18 15:02:03 +0930

    an article

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const body = document.body;
+const share = document.getElementById("share");
 
 window.addEventListener("scroll", () => {
 	if (window.scrollY > 0) {
@@ -6,4 +7,12 @@ window.addEventListener("scroll", () => {
 	} else {
 		body.classList.toggle("down", false);
 	}
+});
+
+share.addEventListener("click", () => {
+	navigator.clipboard.writeText(location.href).then(() => {
+		alert("Link copied to clipboard");
+	}).catch(() => {
+		alert("Failed to copy link.");
+	});
 });
