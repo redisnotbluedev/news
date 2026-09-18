e97f305a8178d18f5863390086916f3d92d6b877
redisnotbluedev <red@redisnot.blue>
2026-09-18 10:10:53 +0930

    finally made it to the good header

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const body = document.body;
 
 window.addEventListener("scroll", () => {
-	if (body.scrollTop > 0) {
+	if (window.scrollY > 0) {
 		body.classList.toggle("down", true);
 	} else {
 		body.classList.toggle("down", false);
